bbf6222a3f647e275d0c5460297e9706877f68f8
Konstantin Kalbazov <kalbazov@gmail.com>
2016-03-21 00:59:13 +0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "ecmaVersion": 6,
+    "rules": {
+        "semi": 0,
+        "strict": 0
+    },
+    "plugins": [
+        "react"
+    ],
+    "env": {
+      "es6": true,
+      "browser": true,
+      "node": true
+    }
+};
